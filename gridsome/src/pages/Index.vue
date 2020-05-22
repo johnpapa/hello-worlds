c832af20645b56ef3f6ab4256527125bff5c637f
Brian Swank <brianswank@gmail.com>
2020-05-21 20:00:36 -0400

    initial commit

---
+++ gridsome/src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+    <div>Hello {{ value }}</div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!"
+  },
+  data() {
+    return {
+      value: "World"
+    };
+  }
+};
+</script>
