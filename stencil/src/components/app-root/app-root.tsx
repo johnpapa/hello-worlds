22dccedaccd23e8053c91c995800c39d44cc06e5
John Papa <john@johnpapa.net>
2020-05-23 08:54:35 -0400

    .

--- stencil/src/components/app-root/app-root.tsx
+++ stencil/src/components/app-root/app-root.tsx
@@ -1,17 +1,13 @@
-import { Component, h } from '@stencil/core';
-
+import { Component, h } from "@stencil/core";
 
 @Component({
-  tag: 'app-root',
-  styleUrl: 'app-root.css',
-  shadow: true
+  tag: "app-root",
+  shadow: true,
 })
 export class AppRoot {
-  name = 'World';
+  name = "World";
 
   render() {
-    return (
-      <div>Hello {this.name}</div>
-    );
+    return <div>Hello {this.name}</div>;
   }
 }
