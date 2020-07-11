d7a235114c5e9efdb9af3ef6f1ed38312c04ed8a
aelgasser <abderraouf.elgasser@gmail.com>
2020-07-11 10:10:31 +0200

    Added Elm implementation (#114)
    * Added Elm implementation
    
    * Moved from yarn to npm
    
    as per https://github.com/johnpapa/hello-worlds/pull/114#issuecomment-656614941
    
    * Updated global readme file
    
    as per https://github.com/johnpapa/hello-worlds/pull/114#issuecomment-656614941
    
    * build script now generates a production build
    
    as per https://github.com/johnpapa/hello-worlds/pull/114#issuecomment-656614941

---
+++ elm/src/Main.elm
@@ -0,0 +1,50 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, div, text)
+import Platform.Cmd exposing (none)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+init : Model
+init =
+    0
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp ->
+            model
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text "Hello World" ]
